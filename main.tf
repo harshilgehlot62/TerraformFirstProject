6ca01343a880eaa6a63686851afea8854b33aa11
Harshil Gehlot <harshilgehlot@Harshils-MacBook-Air.local>
2024-02-15 22:27:06 +0530

    main.tf file added

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "ec2_instance" {
+  #count         = 2
+  ami           = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t2.micro"
+  #availability_zone = element(["us-east-1a", "us-east-1b"], count.index)  # Specify different AZs for each instance
+  key_name      = "mymac"
+}
