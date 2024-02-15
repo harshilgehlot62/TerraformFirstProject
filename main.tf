f7c9433ab2c8b9910e0a5dff2bb5349ce15d428c
Harshil Gehlot <harshilgehlot@Harshils-MacBook-Air.local>
2024-02-15 22:55:02 +0530

    main.tf updated

--- main.tf
+++ main.tf
@@ -6,7 +6,9 @@ resource "aws_instance" "ec2_instance" {
   #count         = 2
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
-  tags =  "TerraformusingJenkins"
+  tags = {
+       name = "TerraformusingJenkins"
+  }
   #availability_zone = element(["us-east-1a", "us-east-1b"], count.index)  # Specify different AZs for each instance
   key_name      = "mymac"
 }
